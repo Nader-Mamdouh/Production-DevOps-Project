308a85884a4c1817a92b8f2e406e3f710c1327a1
Nader-Mamdouh <nadermamdouh20@gmail.com>
2025-11-16 23:58:46 +0200

    Add /health to the code

--- vote/app.py
+++ vote/app.py
@@ -45,7 +45,10 @@ def hello():
     ))
     resp.set_cookie('voter_id', voter_id)
     return resp
-
+    
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
