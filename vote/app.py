923a9bfea54e104d8afb3484dacb02e3f574edb3
Nader-Mamdouh <nadermamdouh20@gmail.com>
2025-11-22 00:46:47 +0200

    test: update app.py

--- vote/app.py
+++ vote/app.py
@@ -46,7 +46,7 @@ def hello():
     resp.set_cookie('voter_id', voter_id)
     return resp
 
- # test the health of the vote service should return 200 ok :)
+ # test the health of the vote service should return 200 ok :) :(
 @app.route("/health")
 def health():
     return "OK", 200
