a575256968dcb999a32197a1648ea8f233dde113
Nader-Mamdouh <nadermamdouh20@gmail.com>
2025-11-22 01:34:08 +0200

    test: updat almost done testing ci-cd

--- vote/app.py
+++ vote/app.py
@@ -46,7 +46,7 @@ def hello():
     resp.set_cookie('voter_id', voter_id)
     return resp
 
- # test the health of the vote service 
+ # test the health of the vote service 200
 @app.route("/health")
 def health():
     return "OK", 200
